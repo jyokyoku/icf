6e78bfcc0ba1a6257d425147de5c7171a95e7476
Masayuki Ietomi <jyokyoku@gmail.com>
2012-08-26 22:30:43 +0900

    icf_dumpメソッドを追加

--- icf-functions.php
+++ icf-functions.php
@@ -9,6 +9,35 @@
 
 require_once dirname(__FILE__) . '/icf-loader.php';
 
+function icf_dump()
+{
+	$backtrace = debug_backtrace();
+
+	if (strpos($backtrace[0]['file'], 'icf/icf-functions.php') !== FALSE) {
+		$callee = $backtrace[1];
+
+	} else {
+		$callee = $backtrace[0];
+	}
+
+	$arguments = func_get_args();
+
+	echo '<div style="font-size: 13px;background: #EEE !important; border:1px solid #666; color: #000 !important; padding:10px;">';
+	echo '<h1 style="border-bottom: 1px solid #CCC; padding: 0 0 5px 0; margin: 0 0 5px 0; font: bold 120% sans-serif;">' . $callee['file'] . ' @ line: ' . $callee['line'] . '</h1>';
+	echo '<pre style="overflow:auto;font-size:100%;">';
+
+	$count = count($arguments);
+
+	for ($i = 1; $i <= $count; $i++) {
+		echo '<strong>Variable #' . $i . ':</strong>' . PHP_EOL;
+		var_dump($arguments[$i - 1]);
+		echo PHP_EOL . PHP_EOL;
+	}
+
+	echo "</pre>";
+	echo "</div>";
+}
+
 function icf_filter(array $array, $key, $default = null)
 {
 	$keys = is_array($key) ? $key : array($key => $default);
