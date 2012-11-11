b2458cd07fa0397fb0e4157ef2cfa1c854ec9b61
Masayuki Ietomi <jyokyoku@gmail.com>
2012-11-12 07:09:14 +0900

    作成したコンポーネントに後からIDを使ってもアクセスできない不具合を修正

--- icf-settingspage.php
+++ icf-settingspage.php
@@ -482,7 +482,7 @@ class ICF_SettingsPage_Section
 
 		} else {
 			$component = new ICF_SettingsPage_Section_Component($id, $title, $this->_page_slug, $this->_id);
-			$this->_components[] = $component;
+			$this->_components[$id] = $component;
 		}
 
 		return $component;
